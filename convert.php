d618263c39f55bc90180b12354e5e1acbbec467a
Gabriel Poma <gpoma@24eme.fr>
2020-12-28 16:34:51 +0100

    Script conversion json en texte utilisable
    Pour l'instant, sortie stdin
    Transformation en csv possible

---
+++ convert.php
@@ -0,0 +1,54 @@
+<?php
+
+if (count($argv) !== 2) {
+    exit('Mauvais nombre d\'argument'.PHP_EOL);
+}
+
+if (file_exists($argv[1]) === false) {
+    exit('Mauvais fichier'.PHP_EOL);
+}
+
+$file = file_get_contents($argv[1]);
+$json = json_decode($file);
+
+if ($json === false) {
+    exit('Mauvais json'.PHP_EOL);
+}
+
+function read_groups($groups)
+{
+    // 1 to 11
+    $index = 1;
+    foreach ($groups as $group) {
+        echo $index.'. '.$group->title.PHP_EOL;
+        yield $group->group->answers;
+        echo PHP_EOL;
+        $index++;
+    }
+}
+
+function read_questions($questions)
+{
+    // a to z
+    $alpha = range('aa', 'zz');
+    $index = 1;
+    foreach ($questions as $question) {
+        echo "\t".$alpha[$index - 1].'. '.$question->title.' ';
+        yield $question;
+
+        if ($index === 26) {$index = 0;}
+        $index++;
+    }
+}
+
+foreach (read_groups($json->answers) as $questions) {
+    foreach (read_questions($questions) as $answers) {
+        $type = $answers->type;
+        if ($type === 'multiple_choice') {
+            $vals = implode('|', $answers->$type->choices);
+            echo $vals.PHP_EOL;
+        } else {
+            echo $answers->$type->value.PHP_EOL;
+        }
+    }
+}
